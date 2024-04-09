4ec508469084f37e958c52a44032760b32cd6800
Bryan10092007 <163545226+Bryan10092007@users.noreply.github.com>
2024-04-09 09:33:42 -0300

    Add files via upload

---
+++ Aula 2/aula2b.php
@@ -0,0 +1,12 @@
+<?php
+$nome = "Bryan";
+$sobrenome = "Faria Rodrigues";
+
+
+echo ("<strong>Nome:</strong> " .$nome. "<br>");
+echo ("<strong>Sobrenome:</strong> " .$sobrenome. "<br>");
+
+echo ("<strong>Nome completo:</strong> ". $nome. " ". $sobrenome. "<br>");
+
+echo"<h2>PHP is fun!</h2>";
+?>

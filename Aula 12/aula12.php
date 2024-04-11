31802cfde7fb165107afb8fe5b18b14302012ae7
Bryan10092007 <163545226+Bryan10092007@users.noreply.github.com>
2024-04-11 11:56:52 -0300

    Add files via upload

---
+++ Aula 12/aula12.php
@@ -0,0 +1,23 @@
+<?php
+$car = [
+    0 => "Volvo",
+    1 => "BMW",
+    2 => "Toyota",
+];
+echo $car["0"];
+echo "<br>";
+echo $car["1"];
+$myCar = [
+    "brand" => "Ford",
+    "model" => "Mustang",
+    "year" => 1964,
+];
+$cars = [];
+$cars[0] = "Volvo";
+$cars[1] = "BMW";
+$cars[2] = "Toyota";
+echo "<br>";
+echo "################################################";
+echo "<br>";
+
+?>
